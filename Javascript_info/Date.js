50c08e54ebf73fed35171609da673f6264cc32bc
Harshavardhanan <harshavardhan2990@gmail.com>
2018-12-28 20:28:03 +0530

    Deleted Duplicates

--- Javascript_info/Date.js
+++
@@ -1,5 +0,0 @@
-let date = new Date("2012-02-20T03:12+00:00");
-console.log(date);
-console.log(date.getFullYear());
-console.log(date.setMonth(11));
-console.log(date);
--- Javascript_info/json.js
+++
@@ -1,9 +0,0 @@
-let user = {
-    name: "John Smith",
-    age: 35
-};
-let stringify = JSON.stringify(user);
-console.log(stringify);
-
-let object = JSON.parse(stringify);
-console.log(object);
