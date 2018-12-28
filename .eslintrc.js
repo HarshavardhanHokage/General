f13a1bc5dec4613d33119da8fd238f2b6c60c820
Harshavardhanan <harshavardhan2990@gmail.com>
2018-12-28 07:36:08 +0530

    Testings from Javascript Info

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "extends": "airbnb-base",
-    rules: {
-        "no-console": 'off',
-        "linebreak-style": 0
-      }
-};
---
+++ Javascript_info/test.js
@@ -0,0 +1,7 @@
+// let text = "list-style-image";
+
+// let arr = text.split("-");
+// arr.push(...(arr.splice(1).map((item)=> {
+//    return item.charAt(0).toUpperCase() + item.slice(1);
+// })));
+// console.log(arr.join(""));
